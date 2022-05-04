631c59e81bb383af408278d578e0380e75ffd9cd
temoana432 <SteveClarke40@hotmail.com>
2022-05-04 10:53:04 -0600

    Dev tool initial commit

---
+++ lib/devcamp_view_tool/renderer.rb
@@ -0,0 +1,8 @@
+module DevcampViewTool
+  class Renderer 
+    def self.copyright name, msg
+      # Use the html copyright symbol
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
