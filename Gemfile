f3c1be1e99cb91483a47d45b0f0246b2a734fb55
temoana432 <SteveClarke40@hotmail.com>
2022-05-04 11:10:12 -0600

    rubygem work

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in devcamp_view_tool.gemspec
 gemspec
--- lib/devcamp_view_tool.rb
+++ lib/devcamp_view_tool.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
-require_relative "devcamp_view_tool/version"
+require "devcamp_view_tool/version"
+require "devcamp_view_tool/renderer"
 
 module DevcampViewTool
   class Error < StandardError; end
