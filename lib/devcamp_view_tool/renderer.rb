88688fa9081bff58d31ebcb045abb174b739cef6
temoana432 <SteveClarke40@hotmail.com>
2022-05-05 09:33:08 -0600

    Generate gem and update readme

--- lib/devcamp_view_tool/renderer.rb
+++ lib/devcamp_view_tool/renderer.rb
@@ -2,7 +2,7 @@ module DevcampViewTool
   class Renderer 
     def self.copyright name, msg
       # Use the html copyright symbol
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      "Fuck Yeah &copy; #{Time.now.year} | <b>#{name}</b> #{msg} ".html_safe
     end
   end
 end
